de3b43adca20fc6222e3fa8d1117773b330da649
Jagadeesh0606 <139302383+Jagadeesh0606@users.noreply.github.com>
2026-09-20 09:23:06 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "k8demowithveeraecommerce"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
