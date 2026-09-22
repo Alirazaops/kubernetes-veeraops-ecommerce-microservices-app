00650d0e1ca96fe381a3438a14051068b3306ce6
Alirazaops <aatirazaops@gmail.com>
2026-09-22 15:44:51 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "k8demowithveeraecommerce"
+    bucket       = "devncloudtechdevops1111111111"
     key          = "terraform.tfstate"
     region       = "us-west-2"
     use_lockfile = true
